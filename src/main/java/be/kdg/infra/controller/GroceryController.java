81801061fc2e852dbe44833ce6530ad61a8680e9
Viktoria Dobisova <viktoria.dobisova@student.kdg.be>
2025-05-24 18:11:02 +0200

    seperating frontend and backend

--- src/main/java/be/kdg/infra/controller/GroceryController.java
+++ src/main/java/be/kdg/infra/controller/GroceryController.java
@@ -1,8 +1,5 @@
 package be.kdg.infra.controller;
 
-import be.kdg.infra.domain.Category;
-import be.kdg.infra.domain.GroceryItem;
-import be.kdg.infra.service.GroceryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +7,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class GroceryController {
 
-    private final GroceryService groceryService;
-
-    public GroceryController(GroceryService groceryService) {
-        this.groceryService = groceryService;
-    }
-
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("items", groceryService.getAllItems());
-        model.addAttribute("newItem", new GroceryItem());
-        model.addAttribute("categories", Category.values());
         return "index";
     }
 }
